99803711ee5e4b6778424f0c64cab3822b2bcd9d
dlancer <dmdpost@gmail.com>
2020-05-02 23:45:16 +0500

    update python and packages

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-PY3_IMAGE=3.6.7-slim-stretch
-GOSU_VER=1.11
+IMAGE_VERSION=3.6.10
+PY3_IMAGE=${IMAGE_VERSION}-slim-buster
+GOSU_VER=1.12
 GOSU_KEY=B42F6819007F00F88E364FD4036A9C25BF357DD4
-TINI_VER=v0.18.0
+TINI_VER=v0.19.0
 TINI_KEY=595E85A6B1B4779EA4DAAEC70B588DFF0527A9B7
 TINI_SUBREAPER=true
 
@@ -17,6 +18,7 @@ END
 )
 
 docker build \
+       --build-arg IMAGE_VERSION=${IMAGE_VERSION} \
        --build-arg PY3_IMAGE=${PY3_IMAGE} \
        --build-arg GOSU_VER=${GOSU_VER} \
        --build-arg GOSU_KEY=${GOSU_KEY} \
@@ -24,4 +26,4 @@ docker build \
        --build-arg TINI_KEY=${TINI_KEY} \
        --build-arg TINI_SUBREAPER=${TINI_SUBREAPER} \
        --build-arg KEY_SERVERS="${KEY_SERVERS}" \
-       --tag digitdroid/python3 ./debian
+       --tag digitdroid/python3:${IMAGE_VERSION} ./debian
